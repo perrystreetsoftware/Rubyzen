a86e63ddab861882a06920f4a7341f0a74b08f2a
Stelios Frantzeskakis <stelios@perrystreet.com>
2024-12-12 17:26:56 +0200

    Show violators in legacy files lint rule

--- lib/rubyzen/matchers/have_maximum_line_count.rb
+++ lib/rubyzen/matchers/have_maximum_line_count.rb
@@ -2,16 +2,22 @@ require_relative 'matcher_helpers'
 
 RSpec::Matchers.define :have_maximum_line_count do |max_lines, custom_message=nil|
   include Rubyzen::Matchers::MatcherHelpers
-  
+
   match do |file_path|
     @custom_message = custom_message
     @max_lines = max_lines
     @file_path = file_path
+    @offenders = []
 
     return false unless @file_path && File.exist?(@file_path)
 
     @actual_count = File.readlines(@file_path).size
-    @actual_count <= @max_lines
+    if @actual_count <= @max_lines
+      true
+    else
+      @offenders << @file_path
+      false
+    end
   end
 
   failure_message do |file_path|
