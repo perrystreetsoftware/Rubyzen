26d2bccaf0d553c4d4b13d6313a4119c09e1b697
Stelios Frantzeskakis <stelios@perrystreet.com>
2024-12-12 17:26:39 +0200

    Fix file path in spec files matcher

--- lib/rubyzen/matchers/have_corresponding_spec_files_in.rb
+++ lib/rubyzen/matchers/have_corresponding_spec_files_in.rb
@@ -2,7 +2,7 @@ require_relative 'matcher_helpers'
 
 RSpec::Matchers.define :have_corresponding_spec_files_in do |spec_root, custom_message=nil|
   include Rubyzen::Matchers::MatcherHelpers
-  
+
   match do |source_files|
     @custom_message = custom_message
     @spec_root = spec_root
@@ -27,8 +27,8 @@ RSpec::Matchers.define :have_corresponding_spec_files_in do |spec_root, custom_m
   end
 
   def corresponding_spec_file(source_file)
-    relative_path = source_file.split('models/').last
+    relative_path = source_file.split('src/').last
     spec_filename = relative_path.sub(/\.rb$/, '_spec.rb')
-    File.join(File.expand_path("../../#{@spec_root}", __dir__), spec_filename)
+    File.join(File.expand_path("../../../sample_project", __dir__), @spec_root, spec_filename)
   end
 end
