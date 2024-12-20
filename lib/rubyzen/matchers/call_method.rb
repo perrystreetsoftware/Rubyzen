d54d1d62401610470e5ba295e41c29a508b09ea9
Stelios Frantzeskakis <stelios@perrystreet.com>
2024-12-20 12:34:52 +0200

    Expand call_method matcher to take receivers as well

--- lib/rubyzen/matchers/call_method.rb
+++ lib/rubyzen/matchers/call_method.rb
@@ -1,24 +1,46 @@
 require_relative 'matcher_helpers'
 
-RSpec::Matchers.define :call_method do |method_name, custom_message=nil|
+RSpec::Matchers.define :call_method do |method_names, on_receivers: nil, message: nil|
   include Rubyzen::Matchers::MatcherHelpers
 
   match do |classes_collection|
-    @custom_message = custom_message
-    @method_name = method_name.to_s
+    @custom_message = message
+
+    @method_names = Array(method_names).map { |m| m.to_s.sub(/^:/, '') }
+
+    @on_receivers = if on_receivers
+                      if on_receivers.respond_to?(:map)
+                        on_receivers.map(&:name)
+                      else
+                        Array(on_receivers)
+                      end
+                    else
+                      nil
+                    end
 
     @offenders = classes_collection.select do |class_info|
-      class_info.called_method_names.include?(@method_name)
+      if @on_receivers
+        class_info.call_sites.any? do |call_site|
+          @method_names.include?(call_site[:method_name]) &&
+            @on_receivers.include?(call_site[:receiver])
+        end
+      else
+        (@method_names & class_info.called_method_names).any?
+      end
     end.map(&:name)
 
     !@offenders.empty?
   end
 
   failure_message do |classes_collection|
-    message_for_failure("Expected some classes to call `#{@method_name}`, but none did.")
+    methods_list = @method_names.size > 1 ? @method_names.join(', ') : @method_names.first
+    receiver_msg = @on_receivers ? " on #{@on_receivers.join(', ')}" : ""
+    message_for_failure(@custom_message || "Expected some classes to call `#{methods_list}`#{receiver_msg}, but none did.")
   end
 
   failure_message_when_negated do |classes_collection|
-    message_for_failure("Expected no classes to call `#{@method_name}`, but these classes did: #{@offenders.join(', ')}")
+    methods_list = @method_names.size > 1 ? @method_names.join(', ') : @method_names.first
+    receiver_msg = @on_receivers ? " on #{@on_receivers.join(', ')}" : ""
+    message_for_failure(@custom_message || "Expected no classes to call `#{methods_list}`#{receiver_msg}, but these classes did: #{@offenders.join(', ')}")
   end
 end
