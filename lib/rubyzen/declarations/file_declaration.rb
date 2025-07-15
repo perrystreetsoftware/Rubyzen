589018da5c99d941d9d170fcf4e34265d47cbf96
Bastian Koell <bastian.koell@gmail.com>
2025-07-15 15:55:05 +0200

    remove comments

--- lib/rubyzen/declarations/file_declaration.rb
+++ lib/rubyzen/declarations/file_declaration.rb
@@ -17,12 +17,10 @@ module Rubyzen
       end
 
       def top_level_module_name
-        # If the top-level AST node itself is a module, get its name
         if ast.type == :module
           return ast.children[0].const_name if ast.children[0]&.respond_to?(:const_name)
         end
 
-        # Otherwise, look for module nodes among the children
         module_node = ast.children.find { |child| child.is_a?(RuboCop::AST::Node) && child.type == :module }
         return unless module_node
         module_node.const_name
