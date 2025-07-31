aa96e73ee86a3ab48893276d1d53964c3bc5ca57
Eric Silverberg <esilverberg@users.noreply.github.com>
2025-07-31 07:49:44 -0400

    Add new methods to support call site (#11)
    Co-authored-by: Adam Tucker <adamt@perrystreet.com>

--- lib/rubyzen/collections/call_site_collection.rb
+++ lib/rubyzen/collections/call_site_collection.rb
@@ -3,6 +3,13 @@ require_relative './base_collection'
 module Rubyzen
   module Collections
     class CallSiteCollection < BaseCollection
+      def with_receiver(receiver)
+        filter { |call_site| call_site.receiver == receiver }
+      end
+
+      def with_method_name(method_name)
+        filter { |call_site| call_site.method_name == method_name }
+      end
     end
   end
 end
---
+++ sample_project/spec/services/include_remote_addr_in_calls_to_deliver_lint_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require 'rubyzen'
+require_relative '../spec_helper'
+
+RSpec.describe 'Make sure to include remote_addr: as a param to Relay.deliver' do
+  context "given a service" do
+    let(:target_call_sites) do
+      (jobs + services).all_methods.call_sites
+    end
+
+    context 'that calls Adapters::Infra::Relay.deliver' do
+      let(:deliver_call_sites) do
+        target_call_sites
+          .with_receiver('Adapters::Infra::Relay')
+          .with_method_name('deliver')
+      end
+
+      it "includes a remote_addr if it has a request_guid when delivering" do
+        expect(deliver_call_sites.filter do |site|
+          !site.keyword_args.include?(:remote_addr) &&
+            site.keyword_args.include?(:request_guid)
+        end).to be_empty
+      end
+    end
+  end
+end
