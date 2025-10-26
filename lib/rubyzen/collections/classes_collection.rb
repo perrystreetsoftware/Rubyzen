df9fabd791ec5d5d2fc7b3a374efde3d335677bc
Stelios Frantzeskakis <stelios@perrystreet.com>
2025-10-27 01:37:23 +0200

    Add filtering in ClassesCollection for macros

--- lib/rubyzen/collections/classes_collection.rb
+++ lib/rubyzen/collections/classes_collection.rb
@@ -16,8 +16,17 @@ module Rubyzen
         AttributesCollection.new(all_attributes)
       end
 
+      def macros
+        all_macros = flat_map(&:macros)
+        MacrosCollection.new(all_macros)
+      end
+
       def with_parent_prefix(prefix)
         filter { |klass| klass.superclass_prefix?(prefix) }
+      end
+
+      def with_macro_name(macro_name)
+        filter { |klass| klass.macros.with_name(macro_name).any? }
       end
 
       def +(other)
